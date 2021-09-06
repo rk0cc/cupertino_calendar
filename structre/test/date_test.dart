78a39d9b2b696dd4e2920c5eff09e80af42575e2
Cyrus Chan <cyruschan1212@gmail.com>
2021-09-06 22:19:58 +0800

    Add more exprect

--- structre/test/date_test.dart
+++ structre/test/date_test.dart
@@ -5,6 +5,8 @@ void main() {
   test("YearMonth compare", () {
     expect(YearMonth(2021, 9).compareTo(YearMonth(2021, 8)), -1);
     expect(YearMonth(2021, 9).compareTo(YearMonth(2020, 8)), -13);
+    expect(YearMonth(2021, 9).compareTo(YearMonth(2021, 12)), 3);
+    expect(YearMonth(2021, 9).compareTo(YearMonth(2023, 3)), 18);
   });
   test("Get a set of YearMonth", () {
     YearMonth f = YearMonth(2021, 8), t = YearMonth(2022, 2);
