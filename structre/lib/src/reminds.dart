c3d6575a0647498ea3b45b11addee709f34a6298
Cyrus Chan <cyruschan1212@gmail.com>
2021-09-07 01:39:37 +0800

    Add list for date remind

--- structre/lib/src/reminds.dart
+++ structre/lib/src/reminds.dart
@@ -1,5 +1,35 @@
+import 'dart:collection';
+import 'dart:html';
+
 import 'package:intl/intl.dart';
 import 'date.dart' show getDTString;
 part 'reminds/abstract.dart';
 part 'reminds/events.dart';
 part 'reminds/holiday.dart';
+
+typedef DateRemindObjectResolver = void Function(List<DateRemind> dr);
+
+class DateRemindList extends ListBase<DateRemind> {
+  final List<DateRemind> _drl;
+
+  DateRemindList([List<DateRemind> drl = const <DateRemind>[]]) : _drl = drl;
+
+  factory DateRemindList.resolver(DateRemindObjectResolver resolver) {
+    List<DateRemind> dr = [];
+    resolver(dr);
+    return DateRemindList(dr);
+  }
+
+  @override
+  int get length => _drl.length;
+
+  @override
+  DateRemind operator [](int index) => _drl[index];
+
+  @override
+  void operator []=(int index, DateRemind value) => _drl[index] = value;
+
+  @override
+  set length(int newLength) =>
+      throw UnsupportedError("You can't assign new length in this list");
+}
