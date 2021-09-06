050b59d4d8783625381d702219923dc7b95aee64
Cyrus Chan <cyruschan1212@gmail.com>
2021-09-06 22:02:28 +0800

    Comment

--- structre/lib/src/date.dart
+++ structre/lib/src/date.dart
@@ -1,10 +1,19 @@
 import 'dart:collection' show LinkedHashSet, SetBase;
 import 'package:quiver/core.dart' show hash2;
 
+/// Create an object that only contains [year] and [month] only
 class YearMonth implements Comparable<YearMonth> {
+  /// Year which follow [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601)
+  /// standard of the year format
   final int year;
+
+  /// Month between `1` to  `12`
+  ///
+  /// Unlike [DateTime], assign outranged value can not adjust year's value.
+  /// However, throws [AssertionError] insteaded
   final int month;
 
+  /// Create new [YearMonth]'s [year] and [month] with valid value
   const YearMonth(this.year, this.month) : assert(month >= 1 && month <= 12);
 
   /// Inspect [DateTime] object and convert back to [YearMonth]
@@ -34,43 +43,60 @@ class YearMonth implements Comparable<YearMonth> {
 
   bool _checkCompreObject(Object compare) => compare is YearMonth;
 
-  /// Compare [YearMonth] is the same value of [year] and [month]
+  /// Compare this [YearMonth] and [compare] the same
   @override
   bool operator ==(Object compare) =>
       // ignore: test_types_in_equals
       _checkCompreObject(compare) && compareTo(compare as YearMonth) == 0;
 
+  /// Compare this [YearMonth] is greater than [compare]
   bool operator >(Object compare) =>
       _checkCompreObject(compare) && compareTo(compare as YearMonth) > 0;
 
+  /// Combine operator of  [==] and [>]
   bool operator >=(Object compare) =>
       _checkCompreObject(compare) && compareTo(compare as YearMonth) >= 0;
 
+  /// Compare this [YearMonth] is lesser than [compare]
   bool operator <(Object compare) =>
       _checkCompreObject(compare) && compareTo(compare as YearMonth) < 0;
 
+  /// Combine operator of  [==] and [<]
   bool operator <=(Object compare) =>
       _checkCompreObject(compare) && compareTo(compare as YearMonth) <= 0;
 
+  /// Hash code from [Obejct.hashCode], but not design for using [==].
+  ///
+  /// Instead, is specific for [YearMonthRange]
   @override
   int get hashCode => hash2(year.hashCode, month.hashCode);
 
+  /// Implemented from [Comparable] which equal `0` means this and [other] are
+  /// the same, greater than `0` means [other] is greater and lesser than `0`
+  /// means [other] is lesser
+  ///
+  /// It also uses the condition for operator [==], [>], [<], [>=] and [<=]
   @override
   int compareTo(YearMonth other) =>
       ((other.year - this.year) * 12) + (other.month - this.month);
 }
 
-typedef YearMonthForEachHandler = void Function(YearMonth);
-
+/// A range of [YearMonth] with inheritance from [SetBase]
 class YearMonthRange extends SetBase<YearMonth> {
   final Set<YearMonth> _ymr;
 
   static LinkedHashSet<YearMonth> get _ymSetGenerator => LinkedHashSet(
       equals: (ym1, ym2) => ym1 == ym2, hashCode: (ym) => ym.hashCode);
 
+  /// Generate [Set] with absolutly nothing inside
   YearMonthRange.emptySet() : _ymr = _ymSetGenerator;
 
-  YearMonthRange(YearMonth from, YearMonth to) : _ymr = _ymSetGenerator {
+  /// Generate [Set] with all [YearMonth] between [from] and [to]
+  ///
+  /// Throws [AssertionError] if [from] is greater than [to]
+  YearMonthRange(YearMonth from, YearMonth to)
+      : assert(from <= to),
+        _ymr = _ymSetGenerator {
     int beginM = from.month;
     for (int y = from.year; y <= to.year; y++) {
       for (int m = beginM; m <= 12; m++) {
@@ -103,4 +129,14 @@ class YearMonthRange extends SetBase<YearMonth> {
 
   @override
   Set<YearMonth> toSet() => _ymr.toSet();
+
+  /// [toList] with orded by the ascending [YearMonth] value
+  ///
+  /// And default [growable] is false
+  @override
+  List<YearMonth> toList({bool growable = false}) {
+    List<YearMonth> _l = _ymr.toList();
+    _l.sort((a, b) => a.compareTo(b));
+    return List.from(_l, growable: growable);
+  }
 }
