0a193518b984d761b748587010dc26bd0d56c367
Cyrus Chan <cyruschan1212@gmail.com>
2021-09-08 01:00:14 +0800

    Build topbar widget

--- month_view/lib/src/widgets/topbar.dart
+++ month_view/lib/src/widgets/topbar.dart
@@ -1,1 +1,18 @@
 part of 'widgets.dart';
+
+class CalendarTopBar extends StatelessWidget {
+  final YearMonth yearMonth;
+  final void Function() onPrevious;
+  final void Function() onNext;
+
+  CalendarTopBar(
+      {required this.yearMonth,
+      required this.onPrevious,
+      required this.onNext});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
