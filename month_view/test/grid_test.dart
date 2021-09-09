a531f33391331577be21948321f965aa060b077f
Cyrus Chan <cyruschan1212@gmail.com>
2021-09-09 20:34:39 +0800

    Add functional grid widget test

--- month_view/test/grid_test.dart
+++ month_view/test/grid_test.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
+
 import 'package:cupertino_calenar_month_view/src/widgets/widgets.dart'
-    show MonthGrid;
+    show MonthGrid, DayBox;
 import 'package:cupertino_calendar_structre/cupertino_calendar_structre.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,8 +9,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'mockapp.dart';
 
 void main() {
+  var tym = YearMonth(2021, 9);
   group("Applying events and holidays", () {
-    var tym = YearMonth(2021, 9);
     test("only allow holiday in current year and month", () {
       expect(
           () => MonthGrid(tym, holidayInThisMonth: [
@@ -53,6 +55,35 @@ void main() {
                     to: DateTime(2021, 10, 15)))),
           throwsA(isA<AssertionError>()));
     });
-    group("Widget test", () {});
+  });
+  group("Widget test", () {
+    var mA = MockApp(Container(width: 300, height: 600, child: MonthGrid(tym)));
+    int todayIntD = DateTime.now().day;
+    testWidgets("find highlighted day", (WidgetTester tester) async {
+      final String todayDay = todayIntD.toString();
+      await tester.pumpWidget(mA);
+      final todayDBWidget = find.text(todayDay, skipOffstage: false);
+      expect(todayDBWidget, findsOneWidget);
+      final dbContainers =
+          find.ancestor(of: todayDBWidget, matching: find.byType(DayBox));
+      expect(dbContainers, findsOneWidget);
+      await tester.pump();
+      expect(
+          (tester
+                  .widget<Container>(find.descendant(
+                      of: dbContainers, matching: find.byType(Container)))
+                  .decoration as BoxDecoration)
+              .color,
+          CupertinoColors.activeBlue);
+    });
+    testWidgets("tap another day", (WidgetTester tester) async {
+      Random random = Random();
+      await tester.pumpWidget(mA);
+      final aDIM = tym.allDaysInMonth;
+      late DateTime targetTap;
+      do {
+        targetTap = aDIM.elementAt(random.nextInt(aDIM.length));
+      } while (targetTap.day != todayIntD);
+    });
   });
 }
