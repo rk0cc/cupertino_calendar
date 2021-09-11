b6e654546f4aeaf3a3bbf04e763498e52bc31b0d
Cyrus Chan <cyruschan1212@gmail.com>
2021-09-11 16:58:07 +0800

    Add example in month view

---
+++ month_view/example/README.md
@@ -0,0 +1,3 @@
+# cupertino_calendar_month_view_example
+
+Example Flutter project of Cupertino Calendar Month View
---
+++ month_view/example/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MonthViewExample());
+
+class MonthViewExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => CupertinoApp();
+}
