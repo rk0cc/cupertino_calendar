706fce7a6321c25df7e67f572b1e57fe16c3eab8
Cyrus Chan <cyruschan1212@gmail.com>
2021-09-11 17:42:29 +0800

    Coding example package

--- month_view/example/lib/main.dart
+++ month_view/example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cupertino_calendar_month_view/month_view.dart';
+import 'package:cupertino_calendar_structre/structre.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MonthViewExample());
@@ -5,4 +7,21 @@ void main() => runApp(MonthViewExample());
 class MonthViewExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) => CupertinoApp();
+}
+
+class MonthViewDemoWidget extends StatefulWidget {
+  final DateTime demoFrom = DateTime.now().subtract(Duration(days: 90)),
+      demoTo = DateTime.now().add(Duration(days: 90));
+
+  @override
+  State<MonthViewDemoWidget> createState() => MonthViewDemoWidgetState();
+}
+
+class MonthViewDemoWidgetState extends State<MonthViewDemoWidget> {
+  @override
+  Widget build(BuildContext context) =>
+      CupertinoCalendarMonthView.withDateRemind(
+          dateRemindList: DateRemindList([]),
+          yearMonthRange: YearMonthRange(YearMonth.dateTime(widget.demoFrom),
+              YearMonth.dateTime(widget.demoTo)));
 }
