a11b0a319d7044820e48c23766ff24cba16bb0db
Cyrus Chan <cyruschan1212@gmail.com>
2021-09-28 16:38:07 +0800

    Add 2 basic files

---
+++ bundle/lib/src/widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:cupertino_calendar_month_view/month_view.dart';
+import 'package:cupertino_calendar_date_reminds_view/date_reminds_view.dart';
+import 'package:cupertino_calendar_structre/structre.dart';
+
+class CupertinoCalendar extends StatefulWidget {
+  @override
+  State<CupertinoCalendar> createState() => CupertinoCalendarState();
+}
+
+class CupertinoCalendarState extends State<CupertinoCalendar> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
