0c2fc887b11f52b8f2a2d3af405a827329f965b9
Cyrus Chan <cyruschan1212@gmail.com>
2021-09-28 19:55:41 +0800

    Get modulize version

--- bundle/lib/cupertino_calenar.dart
+++ bundle/lib/cupertino_calenar.dart
@@ -1,3 +1,12 @@
 library cupertino_calenar;
 
 export 'package:cupertino_calendar_structre/structre.dart';
+export 'package:cupertino_calendar_month_view/month_view_module.dart'
+    show CalendarTopBarStyle, DayBoxStyle;
+export 'package:cupertino_calendar_date_reminds_view/date_reminds_view.dart'
+    show DateRemindWidgetStyle;
+export 'package:cupertino_calendar_date_reminds_view/module.dart'
+    show SelectDateRemindHandler;
+export 'src/widget.dart';
+export 'src/style.dart';
+export 'src/handlers.dart';
---
+++ bundle/lib/module.dart
@@ -0,0 +1,6 @@
+library cupertino_calenar;
+
+export 'package:cupertino_calendar_date_reminds_view/module.dart';
+export 'package:cupertino_calendar_month_view/month_view_module.dart';
+export 'package:cupertino_calendar_structre/structre.dart';
+export 'package:cupertino_calendar_structre/structre_tools.dart';
